511b25dc973b8a2fef4de3148e79b194bd11260a
cesarmaciel1234 <30880816+cesarmaciel1234@users.noreply.github.com>
2026-07-18 11:18:02 -0300

    feat: Actualizar lista de cortes de Cerdo (eliminar repetidos, agregar recorte)

--- src/jefe/promedios/promedio_motor/cerdo.py
+++ src/jefe/promedios/promedio_motor/cerdo.py
@@ -3,6 +3,6 @@ class PromedioCerdo:
     def get_cortes():
         return [
             ("Pechito", 3, 30), ("Bondiola", 2, 40), ("Carré", 4, 30), ("Matambrito", 1, 40),
-            ("Paleta", 6, 20), ("Jamón (Pierna)", 8, 20), ("Panceta", 3, 30), ("Tocino/Grasa", 2, 0),
-            ("Hueso", 2, -20)
+            ("Paleta", 6, 20), ("Jamón", 8, 20), ("Tocino/Grasa", 2, 0),
+            ("Hueso", 2, -20), ("Recorte", 1, 10)
         ]
