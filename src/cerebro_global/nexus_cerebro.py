58ded9362a67d4004b78fff8eecadcbed92af35e
Cesar Maciel <cesarmaciel1234@users.noreply.github.com>
2026-09-17 00:58:34 -0300

    refactor(nexus): limpiar código duplicado en nexus_cerebro.py (v16.43)

--- src/cerebro_global/nexus_cerebro.py
+++ src/cerebro_global/nexus_cerebro.py
@@ -157,13 +157,3 @@ class CerebroNexus:
     def ejecutar_escalar(q, p):
         from src.base_de_datos.database import db_manager
         return db_manager.execute_scalar(q, p)
-
-    @staticmethod
-    def ejecutar_query(q, p):
-        from src.base_de_datos.database import db_manager
-        return db_manager.execute_query(q, p)
-
-    @staticmethod
-    def ejecutar_escalar(q, p):
-        from src.base_de_datos.database import db_manager
-        return db_manager.execute_scalar(q, p)
